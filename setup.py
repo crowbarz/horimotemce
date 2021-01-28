e49f8022bc35510cdf97b28e84afb58db9a103e3
Crowbar Z <crowbarz@outlook.com>
2021-01-28 15:58:32 +0000

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="horimotemce",
-    version="0.1.1",
+    version="0.1.2",
     author="Crowbar Z",
     author_email="crowbarz@outlook.com",
     description="Bridge MCE IR remote events to a Horimote client",
